dd3fba6e9ebb0b8516d91139fc42d7eec3e87c4b
aphitep <aumaphitep@gmail.com>
2026-01-30 10:54:41 +0700

    add flip card logic

--- memories-card/src/App.jsx
+++ memories-card/src/App.jsx
@@ -26,12 +26,48 @@ const finalCard = cardValues.map((value, index) => ({
   isFlipped: false,
   isMatch: false,
 }));
-
-console.log(finalCard);
 function App() {
   const [cards, setCards] = useState(finalCard);
+  const [flipped, setFlipped] = useState([]);
 
-  const handleCardClick = () => {};
+  const handleCardClick = (card) => {
+    if (card.isFlipped || card.isMatch) {
+      return;
+    }
+    const newCards = cards.map((c) => {
+      if (c.id === card.id) {
+        return { ...card, isFlipped: true };
+      } else {
+        return c;
+      }
+    });
+
+    setCards(newCards);
+
+    const newFlippedCards = [...flipped, card.id];
+    setFlipped(newFlippedCards);
+
+    if (flipped.length === 1) {
+      const firstCard = cards[flipped[0]];
+
+      if (firstCard.value === card.value) {
+        console.log("match");
+      } else {
+        setTimeout(() => {
+          const flippedBackCard = newCards.map((c) => {
+            if (newFlippedCards.includes(c.id) || c.id === card.id) {
+              return { ...c, isFlipped: false };
+            } else {
+              return c;
+            }
+          });
+          setCards(flippedBackCard);
+        }, 1000);
+
+        setFlipped([]);
+      }
+    }
+  };
 
   return (
     <div className="app">
@@ -39,7 +75,9 @@ function App() {
 
       <div className="cards-grid">
         {cards.map((card) => {
-          return <Card card={card} cardClick={handleCardClick} />;
+          return (
+            <Card key={card.id} card={card} handleCardClick={handleCardClick} />
+          );
         })}
       </div>
     </div>
--- memories-card/src/components/Card.jsx
+++ memories-card/src/components/Card.jsx
@@ -1,9 +1,10 @@
-export default function Card({ card, cardClick }) {
+export default function Card({ card, handleCardClick }) {
   return (
-    <div className="card">
-      <div className="card-front" onClick={() => cardClick}>
-        ?
-      </div>
+    <div
+      className={`card ${card.isFlipped ? "flipped" : ""}`}
+      onClick={() => handleCardClick(card)}
+    >
+      <div className="card-front">?</div>
       <div className="card-back">{card.value}</div>
     </div>
   );
